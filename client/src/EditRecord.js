b906f29bdf76c3367bd5c91277aaad42d3657fd6
kelly_maass <kmaass77627@gmail.com>
2024-11-03 19:34:10 -0600

    done

---
+++ client/src/EditRecord.js
@@ -0,0 +1,21 @@
+
+
+function EditRecord() {
+
+    function handleSubmit() {
+        
+    }
+
+    return (
+        <div id="edit">
+            <form onSubmit={handleSubmit}>
+                <input type="text"></input>
+                <button>submit</button>
+            </form>
+
+        </div>
+    )
+
+}
+
+export default EditRecord
